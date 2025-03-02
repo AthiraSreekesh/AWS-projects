5064d241442d59d7dfaded4dd5a2e37262178351
athira.parakkandy@allianz.com <athira.parakkandy@allianz.com>
2025-03-02 17:29:26 +0530

    SG added

--- datasource.tf
+++
@@ -1,6 +0,0 @@
-data "aws_key_pair" "key_name" {
-  filter {
-    name = "tag:Name"
-    values = ["${var.project_name}-${var.environment}"]
-  }
-}
--- main.tf
+++ main.tf
@@ -17,3 +17,99 @@ module "aws_key_pair" {
   environment  = var.environment
 }
 
+resource "aws_security_group" "lamp_sgs" {
+
+  for_each    = toset(var.names)
+  name        = "${var.project_name}-${each.key}-sg"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  tags = {
+    Name = "${var.project_name}-${each.key}"
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+}
+
+
+# SG inbound rule for bastion server:
+# This section defines the security group inbound rules for the bastion server.
+# It specifies the allowed inbound traffic to the bastion host, ensuring secure access.
+# SG inbound rule for bastion server
+
+resource "aws_security_group_rule" "ssh_bastion_sg_rule_dev" {
+  count             = var.environment == "Development" ? 1 : 0
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.lamp_sgs["bastion"].id
+}
+
+resource "aws_security_group_rule" "ssh_bastion_sg_rule_prod" {
+  count             = var.environment == "Production" ? 1 : 0
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  type              = "ingress"
+  cidr_blocks       = ["167.103.6.209/32"]
+  security_group_id = aws_security_group.lamp_sgs["bastion"]
+}
+
+
+# SG ssh inbound rule for web server
+
+resource "aws_security_group_rule" "ssh_webserver_sg_rule_dev" {
+
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  type                     = "ingress"
+  source_security_group_id = aws_security_group.lamp_sgs["bastion"].id
+  security_group_id        = aws_security_group.lamp_sgs["webserver"].id
+}
+
+# SG ssh inbound rule for db server
+resource "aws_security_group_rule" "ssh_db_sg_rule_prod" {
+
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  type                     = "ingress"
+  source_security_group_id = aws_security_group.lamp_sgs["bastion"].id
+  security_group_id        = aws_security_group.lamp_sgs["db"].id
+}
+
+# SG inbound rule for web server
+resource "aws_security_group_rule" "webserver_sg_inbound_rule" {
+  for_each          = toset(var.webserver_sg_ports)
+  from_port         = each.key
+  to_port           = each.key
+  protocol          = "tcp"
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.lamp_sgs["webserver"].id
+}
+
+# SG inbound rule for db server
+
+resource "aws_security_group_rule" "db_server_sg_inbound_rule" {
+  for_each                 = toset(var.db_sg_ports)
+  from_port                = each.key
+  to_port                  = each.key
+  protocol                 = "tcp"
+  type                     = "ingress"
+  source_security_group_id = aws_security_group.lamp_sgs["webserver"].id
+  security_group_id        = aws_security_group.lamp_sgs["db"].id
+}
